c2f21b71436c5d9002e89a49cf619f36493dcfdb
Jerosin93 <jmjpjoseph28593@gmail.com>
2022-09-04 12:11:32 +0530

    if

---
+++ src/com/login/AxisBank.java
@@ -0,0 +1,12 @@
+package com.login;
+
+public class AxisBank {
+	public static void main(String args[]) {
+		int age = 17;
+		if (age == 17) {
+			System.out.println("not eligible");
+		}
+
+	}
+
+}
---
+++ src/com/login/ControlStatements.java
@@ -0,0 +1,16 @@
+package com.login;
+
+public class ControlStatements {
+
+	public static void main(String[] args) {
+
+		int age = 6, height = 160;
+		if (age >= 7 || height >= 150) {
+			System.out.println("Good");
+		} else {
+			System.out.println("Bad");
+		}
+
+	}
+
+}
